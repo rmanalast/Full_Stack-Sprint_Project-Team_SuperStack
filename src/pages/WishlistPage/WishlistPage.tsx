f469dfbdf5bc5e5432a69d4d5a3bb51e997ea228
Raven Manalastas <rmanalastas@rrc.ca>
2025-09-30 01:25:38 -0500

    feature: Add WishListForm component and integrate with WishlistPage (I.2 Form Component).

--- src/pages/WishlistPage/WishlistPage.tsx
+++ src/pages/WishlistPage/WishlistPage.tsx
@@ -1,18 +1,45 @@
 import { useState } from "react";
+import WishListForm from "../../forms/WishListForm/WishListForm";
 
-/* This is the paste of the wishlist page where users can view and manage their saved items. */
+/* This is the wishlist page where users can view and manage their saved notification emails. */
 
 function WishlistPage() {
+  // State to hold notification emails
+  const [notifications, setNotifications] = useState<string[]>([]);
 
+  return (
     <section className="wish-list-page">
       <header>
         <h1>WishList</h1>
       </header>
+
       <main>
         <p>
-          This is the feature page for users to add items to their wish list.
+          This is the feature page for users to add items to their wish list and
+          sign up for notifications.
         </p>
-        {/* I.2 and I.3 components will be added here later */}
+
+        {/* I.2: Form Component */}
+        <WishListForm
+          notifications={notifications}
+          setNotifications={setNotifications}
+        />
+
+        {/* I.3: Element Addition/Removal will go here later */}
+
+        {/* Display the current list of signed-up emails */}
+        <div className="notification-list">
+          <h2>Signed-up Emails</h2>
+          {notifications.length === 0 ? (
+            <p>No emails signed up yet.</p>
+          ) : (
+            <ul>
+              {notifications.map((email, index) => (
+                <li key={index}>{email}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </main>
     </section>
   );
