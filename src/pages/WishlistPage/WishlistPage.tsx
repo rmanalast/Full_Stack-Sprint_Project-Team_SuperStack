c87098868e708a9fc5022dd87685770bb21ad8a4
Raven Manalastas <rmanalastas@rrc.ca>
2025-09-29 19:27:00 -0500

    feature: Add WishListPage component as I.1 Feature Page placeholder.

---
+++ src/pages/WishlistPage/WishlistPage.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+/* This is the paste of the wishlist page where users can view and manage their saved items. */
+
+function WishlistPage() {
+
+    <section className="wish-list-page">
+      <header>
+        <h1>WishList</h1>
+      </header>
+      <main>
+        <p>
+          This is the feature page for users to add items to their wish list.
+        </p>
+        {/* I.2 and I.3 components will be added here later */}
+      </main>
+    </section>
+  );
+}
+
+export default WishlistPage;
